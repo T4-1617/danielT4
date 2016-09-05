4d529a9587818031cc20609705768db0069fe60f
Daniel Eriksson <daniel_eriksson97@hotmail.com>
2016-09-05 19:47:25 +0200

    test.

--- 050916kortlek/Program.cs
+++ 050916kortlek/Program.cs
@@ -10,7 +10,7 @@ namespace _050916kortlek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("test.");
         }
     }
 }
