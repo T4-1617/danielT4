0c89cf5679fca5ed0c9b1aa369cf86fcec77b093
Daniel Eriksson <daniel_eriksson97@hotmail.com>
2016-09-15 15:15:24 +0200

    Initial commit, finished layout

---
+++ 160915PeopleList/160915PeopleList/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace _160915PeopleList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbFirstName = new System.Windows.Forms.TextBox();
+            this.txbLastName = new System.Windows.Forms.TextBox();
+            this.rdbCustomer = new System.Windows.Forms.RadioButton();
+            this.rdbEmployee = new System.Windows.Forms.RadioButton();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbxCustomers = new System.Windows.Forms.ListBox();
+            this.lbxEmployees = new System.Windows.Forms.ListBox();
+            this.lblCustomers = new System.Windows.Forms.Label();
+            this.lblEmployee = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // txbFirstName
+            // 
+            this.txbFirstName.Location = new System.Drawing.Point(83, 11);
+            this.txbFirstName.Name = "txbFirstName";
+            this.txbFirstName.Size = new System.Drawing.Size(190, 20);
+            this.txbFirstName.TabIndex = 2;
+            // 
+            // txbLastName
+            // 
+            this.txbLastName.Location = new System.Drawing.Point(83, 35);
+            this.txbLastName.Name = "txbLastName";
+            this.txbLastName.Size = new System.Drawing.Size(190, 20);
+            this.txbLastName.TabIndex = 3;
+            // 
+            // rdbCustomer
+            // 
+            this.rdbCustomer.AutoSize = true;
+            this.rdbCustomer.Location = new System.Drawing.Point(29, 71);
+            this.rdbCustomer.Name = "rdbCustomer";
+            this.rdbCustomer.Size = new System.Drawing.Size(69, 17);
+            this.rdbCustomer.TabIndex = 4;
+            this.rdbCustomer.TabStop = true;
+            this.rdbCustomer.Text = "Customer";
+            this.rdbCustomer.UseVisualStyleBackColor = true;
+            // 
+            // rdbEmployee
+            // 
+            this.rdbEmployee.AutoSize = true;
+            this.rdbEmployee.Location = new System.Drawing.Point(104, 71);
+            this.rdbEmployee.Name = "rdbEmployee";
+            this.rdbEmployee.Size = new System.Drawing.Size(71, 17);
+            this.rdbEmployee.TabIndex = 5;
+            this.rdbEmployee.TabStop = true;
+            this.rdbEmployee.Text = "Employee";
+            this.rdbEmployee.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(181, 68);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // lbxCustomers
+            // 
+            this.lbxCustomers.FormattingEnabled = true;
+            this.lbxCustomers.Location = new System.Drawing.Point(20, 118);
+            this.lbxCustomers.Name = "lbxCustomers";
+            this.lbxCustomers.Size = new System.Drawing.Size(253, 108);
+            this.lbxCustomers.TabIndex = 7;
+            // 
+            // lbxEmployees
+            // 
+            this.lbxEmployees.FormattingEnabled = true;
+            this.lbxEmployees.Location = new System.Drawing.Point(20, 259);
+            this.lbxEmployees.Name = "lbxEmployees";
+            this.lbxEmployees.Size = new System.Drawing.Size(253, 108);
+            this.lbxEmployees.TabIndex = 8;
+            // 
+            // lblCustomers
+            // 
+            this.lblCustomers.AutoSize = true;
+            this.lblCustomers.Location = new System.Drawing.Point(18, 102);
+            this.lblCustomers.Name = "lblCustomers";
+            this.lblCustomers.Size = new System.Drawing.Size(56, 13);
+            this.lblCustomers.TabIndex = 9;
+            this.lblCustomers.Text = "Customers";
+            // 
+            // lblEmployee
+            // 
+            this.lblEmployee.AutoSize = true;
+            this.lblEmployee.Location = new System.Drawing.Point(18, 243);
+            this.lblEmployee.Name = "lblEmployee";
+            this.lblEmployee.Size = new System.Drawing.Size(53, 13);
+            this.lblEmployee.TabIndex = 10;
+            this.lblEmployee.Text = "Employee";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 391);
+            this.Controls.Add(this.lblEmployee);
+            this.Controls.Add(this.lblCustomers);
+            this.Controls.Add(this.lbxEmployees);
+            this.Controls.Add(this.lbxCustomers);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.rdbEmployee);
+            this.Controls.Add(this.rdbCustomer);
+            this.Controls.Add(this.txbLastName);
+            this.Controls.Add(this.txbFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbFirstName;
+        private System.Windows.Forms.TextBox txbLastName;
+        private System.Windows.Forms.RadioButton rdbCustomer;
+        private System.Windows.Forms.RadioButton rdbEmployee;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbxCustomers;
+        private System.Windows.Forms.ListBox lbxEmployees;
+        private System.Windows.Forms.Label lblCustomers;
+        private System.Windows.Forms.Label lblEmployee;
+    }
+}
+
