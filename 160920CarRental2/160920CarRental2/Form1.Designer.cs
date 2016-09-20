451e2ee92baf4fb4015548d7a5c3182f67ec3c69
Daniel Eriksson <daniel_eriksson97@hotmail.com>
2016-09-20 13:02:29 +0200

    Initial commit, Created GUI and classes.

---
+++ 160920CarRental2/160920CarRental2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace _160920CarRental2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cbxPeopleList = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblPeopleCounter = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(345, 152);
+            this.panel1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 261);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 173);
+            this.listBox1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(163, 261);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(194, 173);
+            this.panel2.TabIndex = 4;
+            // 
+            // cbxPeopleList
+            // 
+            this.cbxPeopleList.FormattingEnabled = true;
+            this.cbxPeopleList.Location = new System.Drawing.Point(146, 12);
+            this.cbxPeopleList.Name = "cbxPeopleList";
+            this.cbxPeopleList.Size = new System.Drawing.Size(207, 21);
+            this.cbxPeopleList.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Jag vill registrera en ny:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(4, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(100, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Cancel";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblPeopleCounter
+            // 
+            this.lblPeopleCounter.AutoSize = true;
+            this.lblPeopleCounter.Location = new System.Drawing.Point(9, 228);
+            this.lblPeopleCounter.Name = "lblPeopleCounter";
+            this.lblPeopleCounter.Size = new System.Drawing.Size(12, 13);
+            this.lblPeopleCounter.TabIndex = 2;
+            this.lblPeopleCounter.Text = "x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 444);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxPeopleList);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblPeopleCounter);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox cbxPeopleList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPeopleCounter;
+    }
+}
+
---
+++ 160920CarRental2/160920CarRental2/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _160920CarRental2
+{
+    public abstract class Person
+    {
+
+    }
+
+    public class Customer : Person
+    {
+    }
+
+    public class Employee : Person
+    {
+    }
+
+    public class Supplier : Person
+    {
+    }
+}
