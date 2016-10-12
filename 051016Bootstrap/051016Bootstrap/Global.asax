75e8f5ea6c41cce5b5a7240a3e065d13b852434f
Daniel Eriksson <daniel_eriksson97@hotmail.com>
2016-10-12 11:31:55 +0200

    Demo during lecture.

---
+++ 051016Bootstrap/051016Bootstrap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_051016Bootstrap.MvcApplication" Language="C#" %>
---
+++ Solution1/AdminGUI/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminGUI
+{
+    public partial class Form1 : Form
+    {
+        masterEntities MyDataBase = new masterEntities();
+        public Form1()
+        {
+            InitializeComponent();
+
+            DisplayArticles();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayArticles()
+        {
+            listBox1.Items.Clear();
+            foreach (var item in MyDataBase.Table)
+            {
+                listBox1.Items.Add(item);
+            }
+            listBox1.DisplayMember = "Headline";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string headline = "Demo headline";
+            string content = "Demon content";
+
+            Table MyArticle = new Table() { Headline = headline, Content = content};
+            MyDataBase.Table.Add(MyArticle);
+            MyDataBase.SaveChanges();
+
+            MessageBox.Show("Goodgood yes.");
+        }
+    }
+}
